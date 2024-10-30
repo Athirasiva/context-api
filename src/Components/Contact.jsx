e08de118b61f6e20344f6d63aa2b5d04f4949bdb
Athirasiva <athirasiva8@gmail.com>
2024-10-30 13:36:58 +0530

    dark mode and loght mode

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,8 +1,10 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { ThemeContext } from '../Context/ThemeContext'
 
 function Contact() {
+  const {darkMode} = useContext(ThemeContext)
   return (
-    <div> 
+    <div className={darkMode ? `container container-dark` : `container container-light`}> 
       <h3>This is Contact Page</h3>
     </div>
   )
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -10,14 +10,14 @@ function Header() {
   const {darkMode,toggleDarkmode} = useContext(ThemeContext)
   return (
     <div>
-     <Navbar bg="dark" data-bs-theme="dark">
+     <Navbar bg="dark"  className='w-100' style={{height:'70px'}}>
         <Container>
             <Link  to={'./'} style={{textDecoration:'none',color:'white'}} className='me-3'>Home</Link>
           <Nav className="me-auto">
             <Link to={'./about'} style={{textDecoration:'none',color:'white'}} className='me-3'>About Us</Link>
             <Link to={'./contact'} style={{textDecoration:'none',color:'white'}}>Contact Us</Link>
           </Nav>
-          <Button variant="primary">Dark mode</Button>
+          <Button variant="primary" onClick={toggleDarkmode}>{darkMode ?'Light mode ' : 'Dark mode'}</Button>
         </Container>
       </Navbar>
   </div>
